dd761558dbfaca9ca2e58288a18285a39440fc2b
queekusme <queekus@hotmail.co.uk>
2013-04-30 19:23:58 +0100

    updated buildnumber forgotten in last commit

--- src/me/queekus/programs/ThePotionGame/ThePotionGame.java
+++ src/me/queekus/programs/ThePotionGame/ThePotionGame.java
@@ -6,7 +6,7 @@ import me.queekus.programs.ThePotionGame.api.*;
 public class ThePotionGame{
 
 	public static void main(String[] args){
-		Version version = new Version(0, 0, 0, 1, DevStates.Pre_Alpha);		
+		Version version = new Version(0, 0, 0, 2, DevStates.Pre_Alpha);		
 		PotGameWindow gui = new PotGameWindow("Tombs " + version.toString(), 800, 600);
 		
 		Cauldron Cauldron = new Cauldron();		
